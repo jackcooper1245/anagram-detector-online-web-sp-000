b1ae204b8fe758cada7b352961d6abe07e8d71a0
Jack Cooper <jackcooper1245@gmail.com>
2020-04-18 10:35:42 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -8,7 +8,7 @@ def initialize(word)
 end
 
 def match
-  @word.split("").sort{|a,b| a <=> b} == @word.split("")
+  @word.split("").sort{|a,b| a <=> b} == @word.split("").sort{|a,b| a <=> b}
 end
 
 end
