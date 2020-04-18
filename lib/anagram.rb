4f3abcc84ab5989997cc5e22c0e842bc6effba99
Jack Cooper <jackcooper1245@gmail.com>
2020-04-18 10:39:59 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -3,12 +3,13 @@ class Anagram
 
 attr_accessor :word
 
-def initialize(word)
-  @word = word
+def initialize(words)
+  @word = words
 end
 
-def match
-  @word.split("").sort{|a,b| a <=> b} == @word.split("").sort{|a,b| a <=> b}
+def match(array_of_words)
+  array_of_words.select do |element|
+  @word.split("").sort == element.split("").sort
 end
 
 end
